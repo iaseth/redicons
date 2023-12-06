0da664f09c105c20e464e191b2116c6feb7332a0
Ankur Seth <iankurseth@gmail.com>
2023-12-06 18:58:35 +0530

    Corrected bug on double spaces

--- demo/src/App/Main/Main.tsx
+++ demo/src/App/Main/Main.tsx
@@ -11,8 +11,9 @@ interface MainProps {
 export default function Main ({}: MainProps) {
 	const [query, setQuery] = React.useState("");
 
-	const filterWords = query.trim().split(" ").map(word => word.trim());
+	const filterWords = query.trim().split(" ").map(word => word.trim()).filter(word => word);
 	const filteredIcons = redicons.filter(icon => {
+		if (filterWords.length === 0) return true;
 		for (const word of filterWords) {
 			if (icon.name.includes(word)) {
 				return true;
@@ -25,7 +26,7 @@ export default function Main ({}: MainProps) {
 		<main className="px-2 py-4 bg-slate-200 min-h-screen">
 			<section className='max-w-5xl mx-auto '>
 				<header className='py-3'>
-					<input className='px-2 py-3 w-full' value={query} onChange={e => setQuery(e.target.value)} />
+					<input className='px-3 py-3 w-full' value={query} onChange={e => setQuery(e.target.value)} />
 				</header>
 
 				<section className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-0.5">
