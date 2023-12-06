93ac80e089a9a28fe4d4f11a716b098221635232
Ankur Seth <iankurseth@gmail.com>
2023-12-06 12:02:34 +0530

    Added demo app, icons work but types don't

---
+++ demo/src/App/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Main from './Main/Main';
+
+
+
+export default function App () {
+
+	return (
+		<div className="">
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
+}
---
+++ demo/src/App/Main/Main.tsx
@@ -0,0 +1,17 @@
+import RedIcon from 'redicons';
+
+
+
+interface MainProps {
+	//
+}
+
+export default function Main ({}: MainProps) {
+	return (
+		<main className="px-2 py-4 bg-zinc-100">
+			<section className="">
+				<RedIcon name="0-square-fill" size="32" />
+			</section>
+		</main>
+	);
+}
