3dcde9b47ad019c5665ba5c0780a01029fcb974a
Ankur Seth <iankurseth@gmail.com>
2023-12-06 12:11:37 +0530

    Types are working now but the bundle size is 595.94 kB after gzip

--- demo/src/App/Header/Header.tsx
+++ demo/src/App/Header/Header.tsx
@@ -7,8 +7,8 @@ interface HeaderProps {
 
 export default function Header ({}: HeaderProps) {
 	return (
-		<header className="bg-red-500 text-white px-2 py-5 text-center">
-			<h2>Start</h2>
+		<header className="bg-red-500 text-white px-2 py-4 text-center">
+			<h4>RedIcons</h4>
 		</header>
 	);
 }
--- demo/src/App/Main/Main.tsx
+++ demo/src/App/Main/Main.tsx
@@ -8,9 +8,9 @@ interface MainProps {
 
 export default function Main ({}: MainProps) {
 	return (
-		<main className="px-2 py-4 bg-zinc-100">
-			<section className="">
-				<RedIcon name="0-square-fill" size="32" />
+		<main className="px-2 py-4 bg-slate-200 min-h-screen">
+			<section className="max-w-5xl mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
+				<RedIcon name="0-square-fill" size={32} />
 			</section>
 		</main>
 	);
