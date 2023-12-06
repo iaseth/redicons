9a3c5e129ed2fc3cb919e5a1db73babc28de4d4d
Ankur Seth <iankurseth@gmail.com>
2023-12-06 12:39:15 +0530

    Listing all icons in demo

---
+++ demo/src/App/Main/IconBox/IconBox.tsx
@@ -0,0 +1,22 @@
+import RedIcon from 'redicons';
+
+
+
+interface IconBoxProps {
+	name: string
+}
+
+export default function IconBox ({name}: IconBoxProps) {
+	return (
+		<div className='px-2 py-4 bg-white text-center'>
+			<header className='inline-block mx-auto'>
+				<RedIcon name={name} size={32} />
+			</header>
+			<footer>
+				<h6>
+					<span className='px-1 py-1 rounded bg-zinc-100'>{name}</span>
+				</h6>
+			</footer>
+		</div>
+	);
+}
--- demo/src/App/Main/Main.tsx
+++ demo/src/App/Main/Main.tsx
@@ -1,4 +1,5 @@
-import RedIcon from 'redicons';
+import { redicons } from 'redicons';
+import IconBox from './IconBox/IconBox';
 
 
 
@@ -9,8 +10,8 @@ interface MainProps {
 export default function Main ({}: MainProps) {
 	return (
 		<main className="px-2 py-4 bg-slate-200 min-h-screen">
-			<section className="max-w-5xl mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-				<RedIcon name="0-square-fill" size={32} />
+			<section className="max-w-5xl mx-auto grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-0.5">
+				{redicons.map((redicon, k) => <IconBox key={k} name={redicon.name} />)}
 			</section>
 		</main>
 	);
