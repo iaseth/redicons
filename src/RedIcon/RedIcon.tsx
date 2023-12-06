3e8414f105f0b5f12281364c41976f06e97dcd60
Ankur Seth <iankurseth@gmail.com>
2023-12-06 12:51:35 +0530

    Now RedIcon can accept fill and className as args

--- src/RedIcon/RedIcon.tsx
+++ src/RedIcon/RedIcon.tsx
@@ -10,11 +10,15 @@ import Symbol from "./Symbol";
 interface RedIconProps {
 	name: string,
 	size?: number,
+	className?: string,
+	fill?: string
 }
 
 const RedIcon: FC<RedIconProps> =  ({
 	name,
-	size=16
+	size=16,
+	className="",
+	fill="currentColor"
 }) => {
 	const icon = redicons.find(icon => icon.name === name);
 	if (!icon) {
@@ -27,7 +31,7 @@ const RedIcon: FC<RedIconProps> =  ({
 	};
 
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" className={icon.className} style={style}>
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={fill} viewBox="0 0 16 16" className={className} style={style}>
 			{icon.paths.map((path: PathData) => <Path data={path} />)}
 			{icon.symbols.map((symbol: SymbolData) => <Symbol data={symbol} />)}
 			{icon.circles.map((circle: CircleData) => <Circle data={circle} />)}
